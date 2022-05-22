a5204091807bfc42d631c14ca7cd2544eb5d7435
SUZUKI Kei <todokimasen@gmail.com>
2022-05-22 10:26:40 +0900

    docstring を追加した.

--- src/wsgi_coverage/_wsgi_coverage.py
+++ src/wsgi_coverage/_wsgi_coverage.py
@@ -2,9 +2,34 @@ import coverage
 import glob
 import importlib
 import os
+import types
 
 
 def measured(target: callable) -> callable:
+    """
+        カバレッジを計測する.
+
+        Arguments
+        ---------
+        target: callable
+            target を実行中のカバレッジを計測する.
+
+        Returns
+        -------
+        callable
+            カバレッジを計測する処理でラップした target.
+
+        Examples
+        --------
+        (1) 既存の関数を引数に呼び出す:
+
+            wrapped_target = measured(target)
+
+        (2) 関数を定義するときにデコレータとして指定する:
+
+            @measured
+            def target(): ...
+    """
     def wrapper(*args, **kwargs):
         cov = _new_coverage()
         try:
@@ -16,7 +41,15 @@ def measured(target: callable) -> callable:
     return wrapper
 
 
-def _new_coverage():
+def _new_coverage() -> coverage.Coverage:
+    """
+        新しい coverage.Coverage オブジェクトを生成する.
+
+        Arguments
+        ---------
+        coverage.Coverage
+            新しい coverage.Coverage オブジェクト.
+    """
     root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
     config_file = os.path.join(root_dir, "config", "coveragerc")
     cov = coverage.Coverage(config_file=config_file)
@@ -26,7 +59,18 @@ def _new_coverage():
 
 
 @measured
-def import_submodules(module):
+def import_submodules(module: types.ModuleType) -> list[types.ModuleType]:
+    """
+        指定したモジュールのサブモジュールを全てインポートする.
+
+        インポート処理は measured() によってカバレッジ計測の対象となる.
+        インポートされないモジュールもカバレッジレポートに含めるために使用する.
+
+        Arguments
+        ---------
+        module: list[types.ModuleType]
+            インポートしたサブモジュールのリスト.
+    """
     files = glob.glob(os.path.join(os.path.dirname(module.__file__), '[a-zA-Z0-9]*.py'))
     names = [os.path.split(os.path.splitext(file)[0])[1] for file in files]
     submodules = [importlib.import_module('.'.join([module.__name__, name])) for name in names]
